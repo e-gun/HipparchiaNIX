d293d377347b2670b73d6b0361361edb86adeab1
EG <egun@antisigma.classics.utoronto.ca>
2016-10-24 12:17:45 -0400

    careless update of selfupdate...

--- selfupdate.sh
+++ selfupdate.sh
@@ -4,10 +4,10 @@
 # git fetch --all
 # git reset --hard master
 
-cd ~/venv/HipparchiaServer/ && git init && git pull https://github.com/e-gun/HipparchiaServer.git
-cd ~/venv/HipparchiaBuilder/ && git init && git pull https://github.com/e-gun/HipparchiaBuilder.git
-cd ~/venv/HipparchiaSQLoader/ && git init && git pull https://github.com/e-gun/HipparchiaSQLoader.git
-cd ~/venv/HipparchiaBSD/ && git init && git pull https://github.com/e-gun/HipparchiaBSD.git
+cd ~/venv/HipparchiaServer/ && git pull https://github.com/e-gun/HipparchiaServer.git
+cd ~/venv/HipparchiaBuilder/ && git pull https://github.com/e-gun/HipparchiaBuilder.git
+cd ~/venv/HipparchiaSQLoader/ && git pull https://github.com/e-gun/HipparchiaSQLoader.git
+cd ~/venv/HipparchiaBSD/ && git pull https://github.com/e-gun/HipparchiaBSD.git
 
 cd ~
 ~/circusctl restart
