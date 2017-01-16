e4b6fe12a89ef9f1eceb0a14a8c8e4ec07a3a8aa
EG <egun@antisigma.classics.utoronto.ca>
2017-01-16 09:12:51 -0500

    freeBSD paths updated; python35 --> python36

--- selfupdate.sh
+++ selfupdate.sh
@@ -4,10 +4,10 @@
 # git fetch --all
 # git reset --hard master
 
-cd ~/venv/HipparchiaServer/ && git pull https://github.com/e-gun/HipparchiaServer.git
-cd ~/venv/HipparchiaBuilder/ && git pull https://github.com/e-gun/HipparchiaBuilder.git
-cd ~/venv/HipparchiaSQLoader/ && git pull https://github.com/e-gun/HipparchiaSQLoader.git
-cd ~/venv/HipparchiaBSD/ && git pull https://github.com/e-gun/HipparchiaBSD.git
+cd ~/hipparchia_venv/HipparchiaServer/ && git pull https://github.com/e-gun/HipparchiaServer.git
+cd ~/hipparchia_venv/HipparchiaBuilder/ && git pull https://github.com/e-gun/HipparchiaBuilder.git
+cd ~/hipparchia_venv/HipparchiaSQLoader/ && git pull https://github.com/e-gun/HipparchiaSQLoader.git
+cd ~/hipparchia_venv/HipparchiaBSD/ && git pull https://github.com/e-gun/HipparchiaBSD.git
 
 cd ~
 ~/circusctl restart
