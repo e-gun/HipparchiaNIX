e74157dfca5f5a80379fa6c84b97ea8c96e4ca45
e-gun <egun@antisigma.classics.utoronto.ca>
2020-03-13 19:14:02 -0400

    pip3 install matplotlib==3.1.3 because matplotlib people say 3.2.0 is not broken...

--- pipupgrade.sh
+++ pipupgrade.sh
@@ -1,3 +1,5 @@
 #/bin/bash
+# NB matplotlib==3.2.0 breaks networkx
 ./bin/pip install --upgrade pip
-./bin/pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 ./bin/pip install -U
+# ./bin/pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 ./bin/pip install -U
+./bin/pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | grep -v matplotlib | xargs -n1 ./bin/pip install -U
