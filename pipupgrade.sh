5fbc413c628380be08944fca23b541eedc2989f5
e-gun <egun@antisigma.classics.utoronto.ca>
2020-03-13 19:16:01 -0400

    record URL for matplotlib issue #16739

--- pipupgrade.sh
+++ pipupgrade.sh
@@ -1,5 +1,6 @@
-#/bin/bash
+#!/bin/bash
 # NB matplotlib==3.2.0 breaks networkx
+# see https://github.com/matplotlib/matplotlib/issues/16739
 ./bin/pip install --upgrade pip
 # ./bin/pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 ./bin/pip install -U
 ./bin/pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | grep -v matplotlib | xargs -n1 ./bin/pip install -U
