68cd6f6b7ee082f36992a3e2ef159f2531e43d91
e-gun <egun@antisigma.classics.utoronto.ca>
2020-03-14 13:33:49 -0400

    now safe to load matplotlib 3.2.0

--- pipupgrade.sh
+++ pipupgrade.sh
@@ -2,5 +2,5 @@
 # NB matplotlib==3.2.0 breaks networkx
 # see https://github.com/matplotlib/matplotlib/issues/16739
 ./bin/pip install --upgrade pip
-# ./bin/pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 ./bin/pip install -U
-./bin/pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | grep -v matplotlib | xargs -n1 ./bin/pip install -U
+./bin/pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 ./bin/pip install -U
+# ./bin/pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | grep -v matplotlib | xargs -n1 ./bin/pip install -U
