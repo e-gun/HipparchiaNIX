b90ff3eece3c7a9339f681c490d5674ab34bd8db
e-gun <egun@antisigma.classics.utoronto.ca>
2020-08-10 11:16:27 -0400

    https://github.com/e-gun/HipparchiaNIX.git

--- selfupdate.sh
+++ selfupdate.sh
@@ -7,9 +7,10 @@
 cd ~/hipparchia_venv/HipparchiaServer/ && git pull https://github.com/e-gun/HipparchiaServer.git
 cd ~/hipparchia_venv/HipparchiaBuilder/ && git pull https://github.com/e-gun/HipparchiaBuilder.git
 cd ~/hipparchia_venv/HipparchiaSQLoader/ && git pull https://github.com/e-gun/HipparchiaSQLoader.git
-cd ~/hipparchia_venv/HipparchiaNIX/ && git pull https://github.com/e-gun/HipparchiaBSD.git
+cd ~/hipparchia_venv/HipparchiaNIX/ && git pull https://github.com/e-gun/HipparchiaNIX.git
 cd ~/hipparchia_venv/HipparchiaMacOS/ && git pull https://github.com/e-gun/HipparchiaMacOS.git
 cd ~/hipparchia_venv/HipparchiaThirdPartySoftware/ && git pull https://github.com/e-gun/HipparchiaThirdPartySoftware.git
 cd ~/hipparchia_venv/HipparchiaWindows/ && git pull https://github.com/e-gun/HipparchiaWindows.git
 cd ~/hipparchia_venv/HipparchiaLexicalData/ && git pull https://github.com/e-gun/HipparchiaLexicalData.git
 cd ~
+
