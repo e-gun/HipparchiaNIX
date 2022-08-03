24946d454dea6318afb1f446c9fe02642787c956
e-gun <egun@antisigma.classics.utoronto.ca>
2022-08-03 10:39:22 -0400

    FreeBSD 13.1; + shallow clone of HipparchiaLexicalData

--- selfupdate.sh
+++ selfupdate.sh
@@ -8,9 +8,9 @@ cd ~/hipparchia_venv/HipparchiaServer/ && git pull https://github.com/e-gun/Hipp
 cd ~/hipparchia_venv/HipparchiaBuilder/ && git pull https://github.com/e-gun/HipparchiaBuilder.git
 cd ~/hipparchia_venv/HipparchiaSQLoader/ && git pull https://github.com/e-gun/HipparchiaSQLoader.git
 cd ~/hipparchia_venv/HipparchiaNIX/ && git pull https://github.com/e-gun/HipparchiaNIX.git
-cd ~/hipparchia_venv/HipparchiaMacOS/ && git pull https://github.com/e-gun/HipparchiaMacOS.git
+# cd ~/hipparchia_venv/HipparchiaMacOS/ && git pull https://github.com/e-gun/HipparchiaMacOS.git
 cd ~/hipparchia_venv/HipparchiaThirdPartySoftware/ && git pull https://github.com/e-gun/HipparchiaThirdPartySoftware.git
-cd ~/hipparchia_venv/HipparchiaWindows/ && git pull https://github.com/e-gun/HipparchiaWindows.git
-cd ~/hipparchia_venv/HipparchiaLexicalData/ && git pull https://github.com/e-gun/HipparchiaLexicalData.git
+# cd ~/hipparchia_venv/HipparchiaWindows/ && git pull https://github.com/e-gun/HipparchiaWindows.git
+# cd ~/hipparchia_venv/HipparchiaLexicalData/ && git pull https://github.com/e-gun/HipparchiaLexicalData.git
 cd ~
 
